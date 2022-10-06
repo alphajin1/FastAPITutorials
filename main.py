3eda4d0b1f3dd79aa22ab2a4c54f5246f82c741c
alphajin1 <alphajin1@gmail.com>
2022-10-06 12:07:32 +0900

    튜토리얼

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # command: uvicorn main:app --reload
+    # http://0.0.0.0:8000/docs > swaggerUI
+    # http://127.0.0.1:8000/redoc > ...?
